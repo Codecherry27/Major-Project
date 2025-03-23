2903449f6d5f21c18fe2336dad84fbc35019450f
Soumita Kar <karsoumita0@gmail.com>
2025-03-23 19:49:09 +0530

    Changes in frontpage

--- src/Frontpage/frontpage.js
+++ src/Frontpage/frontpage.js
@@ -24,8 +24,8 @@ export default function BankingFrontPage () {
         <li><a onClick={()=>navigate("/deposit")}>Deposit</a></li>
         <li><a onClick={()=>navigate("/withdraw")}>Withdraw</a></li>
         <li><a onClick={()=>navigate("/transfer")}>Transfer</a></li>
-        <li><a onClick={()=>navigate("/balance")}>Balance</a></li>
-        <li><a onClick={()=>navigate("/logout")}>Logout</a></li>
+        <li><a onClick={()=>navigate("/")}>Balance</a></li>
+        <li><a onClick={()=>navigate("/frontpage")}>Logout</a></li>
     </ul>
 </div>
 
